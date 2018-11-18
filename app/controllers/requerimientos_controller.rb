4b50e7c2025737a6d8124cd7708072f159c14f23
tomasBustamante <bustamantejt@gmail.com>
2018-11-18 14:00:27 -0300

    Requerimientos

--- app/controllers/requerimientos_controller.rb
+++ app/controllers/requerimientos_controller.rb
@@ -1,2 +1,40 @@
 class RequerimientosController < ApplicationController
+  def show
+    @proyecto = Proyecto.find(params[:proyecto_id])
+    @requerimiento = @proyecto.requerimientos.find(params[:id])
+  end
+
+  def edit
+    @proyecto = Proyecto.find(params[:proyecto_id])
+    @requerimiento = @proyecto.requerimientos.find(params[:id])
+  end
+
+  def create
+    @proyecto = Proyecto.find(params[:proyecto_id])
+    @requerimiento = @proyecto.requerimientos.create(requerimiento_params)
+    redirect_to proyecto_path(@proyecto)
+  end
+
+  def destroy
+    @proyecto = Proyecto.find(params[:proyecto_id])
+    @requerimiento = @proyecto.requerimientos.find(params[:id])
+    @requerimiento.destroy
+    redirect_to proyecto_path(@proyecto)
+  end
+
+  def update
+    @proyecto = Proyecto.find(params[:proyecto_id])
+    @requerimiento = @proyecto.requerimientos.find(params[:id])
+
+    if @requerimiento.update(requerimiento_params)
+      redirect_to @requerimiento
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def requerimiento_params
+      params.require(:requerimiento).permit(:nombre, :descripcion)
+    end
 end
--- app/models/requerimiento.rb
+++ app/models/requerimiento.rb
@@ -1,3 +1,5 @@
 class Requerimiento < ApplicationRecord
   belongs_to :proyecto
+  validates :nombre, presence: true, length: { minimum: 5 }
+  validates :descripcion, presence: true, length: { minimum: 10 }
 end
