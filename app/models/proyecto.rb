64d63de6642f85d98901970f866a0388e8b00531
tomasBustamante <bustamantejt@gmail.com>
2018-11-18 00:11:23 -0300

    Proyecto tests

--- app/models/proyecto.rb
+++ app/models/proyecto.rb
@@ -1,3 +1,4 @@
 class Proyecto < ApplicationRecord
   validates :nombre, presence: true, length: { minimum: 5 }
+  validates :descripcion, presence: true, length: { minimum: 10 }
 end
--- spec/models/proyecto_spec.rb
+++ spec/models/proyecto_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Proyecto, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:nombre) }
+  it { is_expected.to validate_length_of(:nombre).is_at_least(5) }
+  it { is_expected.to validate_presence_of(:descripcion) }
+  it { is_expected.to validate_length_of(:descripcion).is_at_least(10) }
 end
