4d9ad2cd6d69e0842180c2c1d4650464fb280ca3
tomasBustamante <bustamantejt@gmail.com>
2018-11-18 02:00:18 -0300

    Welcome test fixed

--- features/step_definitions/cucumber_rails_steps.rb
+++ features/step_definitions/cucumber_rails_steps.rb
@@ -3,5 +3,5 @@ When(/^I go to the homepage$/) do
 end
 
 Then(/^I should see the welcome message$/) do
-  expect(page).to have_content("Seguimiento de proyectos y tareas")
+  expect(page).to have_content("Seguimiento de proyectos")
 end
