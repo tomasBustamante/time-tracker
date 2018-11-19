86fcd533458093d73d51d3c91d1e800ac8325ef3
Tomás Bustamante <bustamantejt@gmail.com>
2018-11-19 17:47:08 -0300

    prueba nombre de recurso

--- spec/models/recurso_spec.rb
+++ spec/models/recurso_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Recurso, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:nombre) }
+  it { is_expected.to validate_length_of(:nombre).is_at_least(5) }
 end
