018ee2d4f2fa5b41664c5a6bfbc21511a6bc4f1b
tomasBustamante <bustamantejt@gmail.com>
2018-11-20 23:14:02 -0300

    Remuevo metodo no probado

--- app/controllers/registro_horas_controller.rb
+++ app/controllers/registro_horas_controller.rb
@@ -11,20 +11,6 @@ class RegistroHorasController < ApplicationController
     redirect_to proyecto_requerimiento_tarea_path(:id => @tarea.id)
   end
 
-  def cargar_horas
-    @proyecto = Proyecto.find(params[:proyecto_id])
-    @requerimiento = @proyecto.requerimientos.find(params[:requerimiento_id])
-    @tarea = @requerimiento.tareas.find(params[:tarea_id])
-
-    begin
-      @tarea.cargar_horas(registro_horas_params[:horas], registro_horas_params[:fecha])
-      self.create
-    rescue StandardError => e
-      puts e
-    end
-
-  end
-
   private
     def registro_horas_params
       params.require(:registro_hora).permit(:horas, :recurso, :fecha)
