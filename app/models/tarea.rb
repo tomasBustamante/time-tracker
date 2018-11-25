6a76c17db63b5ea750789cd9b3c146b901775efc
tomasBustamante <bustamantejt@gmail.com>
2018-11-25 19:54:35 -0300

    tarea fix

--- app/models/tarea.rb
+++ app/models/tarea.rb
@@ -2,17 +2,12 @@ class Tarea < ApplicationRecord
   belongs_to :requerimiento
   has_many :registro_horas, dependent: :destroy
   validates :descripcion, presence: true, length: {
-    minimum: 5,
-    message: "debe tener por lo menos 5 caracteres."
+    minimum: 5
   }
   validates :horas_estimadas, presence: true, numericality: {
     greater_than: 0,
     message: "deben ser mayores a cero."
   }
-  # validates :horas_cargadas, numericality: {
-  #   greater_than_or_equal_to: 0,
-  #   message: "no pueden ser negativas."
-  # }
   validates :horas_cargadas, numericality: {
     less_than_or_equal_to: :horas_estimadas,
     message: "no deben superar a las horas estimadas."
