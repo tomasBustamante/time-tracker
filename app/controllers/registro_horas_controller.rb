2b09168e975b8e8737fd7dce3f59ea0bb84d7ca0
tomasBustamante <bustamantejt@gmail.com>
2018-11-25 21:11:51 -0300

    fix validaciones

--- app/controllers/registro_horas_controller.rb
+++ app/controllers/registro_horas_controller.rb
@@ -9,25 +9,28 @@ class RegistroHorasController < ApplicationController
     @tarea = @requerimiento.tareas.find(params[:tarea_id])
     horas_cargadas_prev = @tarea.horas_cargadas + 0
     horas_cargadas_post = @tarea.horas_cargadas + params['registro_hora']['horas'].to_i
-    @registro_hora = @tarea.registro_horas.create(registro_horas_params)
     suma = 0
     @tarea.registro_horas.each do |registro_hora|
       suma += registro_hora.horas if registro_hora.fecha == params['registro_hora']['fecha'].to_date
     end
-    if suma > 8
-      @registro_hora.errors.add(:horas, 'cargadas: no pueden ser más de ocho por día.')
-    end
-    if !@registro_hora.errors.any?
-      if @tarea.update({"horas_cargadas" => horas_cargadas_post})
-        redirect_to [@tarea.requerimiento.proyecto, @tarea.requerimiento, @tarea]
+    puts suma
+    if (suma + params['registro_hora']['horas'].to_i) > 8
+      @tarea.errors.add(:horas_cargadas, ': no pueden ser más de ocho por día.')
+      render 'tareas/show'
+    else
+      @registro_hora = @tarea.registro_horas.create(registro_horas_params)
+      if !@registro_hora.errors.any?
+        if @tarea.update({"horas_cargadas" => horas_cargadas_post})
+          redirect_to [@tarea.requerimiento.proyecto, @tarea.requerimiento, @tarea]
+        else
+          @tarea.registro_horas.delete(@registro_hora.id)
+          @registro_hora.destroy
+          @tarea.horas_cargadas = horas_cargadas_prev
+          render 'tareas/show'
+        end
       else
-        @tarea.registro_horas.delete(@registro_hora)
-        @registro_hora.destroy
-        @tarea.horas_cargadas = horas_cargadas_prev
         render 'tareas/show'
       end
-    else
-      render 'tareas/show'
     end
   end
 
